f145fc721192e7e9d2145beb723c30e010e30ece
Iry Indriyanto <iry.indriyanto20@gmail.com>
2024-01-10 06:49:21 -0700

    add weather card

---
+++ src/components/WeatherCard.tsx
@@ -0,0 +1,10 @@
+import { Card } from "./ui/card";
+
+const WeatherCard = () => {
+  return (
+    <Card>
+      <div className=" w-full h-[200px]">weather data</div>
+    </Card>
+  );
+};
+export default WeatherCard;
--- src/dashboard/Weather.tsx
+++ src/dashboard/Weather.tsx
@@ -1,11 +1,16 @@
 import SearchBar from "@/components/SearchBar";
+import WeatherCard from "@/components/WeatherCard";
 import { H1 } from "@/components/typography/H1";
 
 const Weather = () => {
   return (
     <div className="py-8 md:w-[800px]">
-        <H1 className=" text-4xl mb-8">Current Weather</H1>
+      <H1 className=" text-4xl mb-8">Current Weather</H1>
+      <div className="flex flex-col gap-6">
+
       <SearchBar />
+      <WeatherCard />
+      </div>
     </div>
   );
 };
