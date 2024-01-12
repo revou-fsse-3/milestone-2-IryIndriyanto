8b65b6a44743dd6ffda200e74581276a6987185e
Iry Indriyanto <iry.indriyanto20@gmail.com>
2024-01-12 01:03:35 -0700

    create H2 component

--- src/components/typography/H2.tsx
+++ src/components/typography/H2.tsx
@@ -1,8 +1,20 @@
-export function H2() {
-    return (
-      <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
-        The People of the Kingdom
-      </h2>
-    )
-  }
-  
+import { cn } from '@/lib/utils'
+import { HTMLAttributes } from 'react'
+
+interface Props extends HTMLAttributes<HTMLHeadingElement> {
+  children: string
+}
+
+export function H2({ className, children, ...props }: Props) {
+  return (
+    <h2
+      className={cn(
+        'scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0',
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </h2>
+  )
+}
